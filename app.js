f264b1b711f7171c6f09d59f098b855b339cab91
DongHyeokJeong <ddkkd1@naver.com>
2017-01-25 00:43:35 +0900

    RestfulAPI

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-// Express를 이용해서 RESTful 서비스 작성
-// 1. 영화 목록과 영화 상세보기 : JSON
-// 2. 영화 리뷰 기능 추가 : JSON
-// 3. 라우터 분리
-// 4. 템플릿 적용
-
 var express = require('express');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-//app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(require('./movieRouter'));
-
-app.get('/', function(req, res) {
-	res.end('Welcome to Movies app');
-});
-
 app.use(handleError);
 
-function handleError(err, req, res, next){
-    console.log("Error", err);
+app.listen(3000);
+
+function handleError(err, req, res, next) {
     res.status(err.code).send({msg:err.message});
 }
-
-app.listen(3000);
